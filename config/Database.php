d48f8510b3c76983491f6fc0eb656f1c6fe73cdd
ncferree <ncferree@gmail.com>
2021-05-07 14:36:37 -0600

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -1,9 +1,9 @@
 <?php
     class Database {
-        private $host = 'localhost';
-        private $db_name = 'quotesdb';
-        private $username = 'root';
-        private $password = '';
+        private $host = 'y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+        private $db_name = 'xwvy9vqpsbr4t2zg';
+        private $username = 'sm1azw3h03hq7ck5';
+        private $password = 's0m5d8d13lpuhn7f';
         private $conn;
 
         public function connect() {
