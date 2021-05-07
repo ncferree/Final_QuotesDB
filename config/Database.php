a9a31916c7ade4ba0f6ee423dc99ac5f19903e3b
ncferree <ncferree@gmail.com>
2021-05-07 15:18:26 -0600

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -1,13 +1,16 @@
 <?php
     class Database {
-        private $host = 'y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-        private $db_name = 'xwvy9vqpsbr4t2zg';
-        private $username = 'sm1azw3h03hq7ck5';
-        private $password = 's0m5d8d13lpuhn7f';
         private $conn;
 
         public function connect() {
-         
+            $url = getenv('JAWSDB_URL');
+            $dbparts = parse_url($url);
+
+            $hostname = $dbparts['host'];
+            $username = $dbparts['user'];
+            $password = $dbparts['pass'];
+            $database = ltrim($dbparts['path'],'/');
+            
             $this->conn = null;
            
 
