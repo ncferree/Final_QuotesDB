558988de39d136ec783c6c5ed1a667f7d7c20b91
ncferree <ncferree@gmail.com>
2021-05-10 15:06:17 -0600

    Update index.php

--- index.php
+++ index.php
@@ -25,45 +25,16 @@
    $limit = filter_input(INPUT_GET, 'limit', FILTER_VALIDATE_INT);
 
    //Get request Quote Data
-   // if ($authorId) {
-   //     $quote->authorId = $authorId;
-   //  }
-   // if ($categoryId) {
-   //     $quote->categoryId = $categoryId; 
-   // }
+    if ($authorId) {
+       $quote->authorId = $authorId;
+    }
+    if ($categoryId) {
+       $quote->categoryId = $categoryId; 
+    }
 
    // Read Data
    $authors = $author->read();
    $categories = $category->read();
-   //$quotes = $quote->read();
-
-    $query = "?";
-    if ($authorId && $categoryId) {
-        $query .= "authorId={$authorId}&categoryId={$categoryId}";
-    } elseif ($authorId) {
-        $query .= "authorId={$authorId}";
-    } elseif ($categoryId) {
-        $query .= "categoryId={$categoryId}";
-    }
-
-    if ($limit) {
-        $query .= "&limit={$limit}";
-    }
-
-
-    $url = "http://final-quotesdb.herokuapp.com/{$query}";
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_NOBODY, 0);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-    $res = curl_exec($ch);
-    curl_close($ch);
-
-    $quotes = json_decode($res, true);
-
+   $quotes = $quote->read();
 
    include('view/quotes_list.php');
